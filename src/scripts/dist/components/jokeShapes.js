f46639d76f1139d1a50c25535499e12d881f3beb
David <ruedarosasdavid@gmail.com>
2025-07-18 19:33:45 +0200

    Compile all ts files and test

---
+++ src/scripts/dist/components/jokeShapes.js
@@ -0,0 +1,10 @@
+"use strict";
+const jokeContainer = document.querySelector('.custom-background');
+export function changeJokeShape() {
+    const shapes = ['shape-1', 'shape-2', 'shape-3', 'shape-4', 'shape-5', 'shape-6'];
+    const currentShape = shapes.find(shape => jokeContainer.classList.contains(shape));
+    const availableShapes = shapes.filter(shape => shape !== currentShape);
+    const newShape = availableShapes[Math.floor(Math.random() * availableShapes.length)];
+    shapes.forEach(shape => jokeContainer.classList.remove(shape));
+    jokeContainer.classList.add(newShape);
+}

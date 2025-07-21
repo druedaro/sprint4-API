587b822ff0698ae9f8d17b14780f00da8cfbd27e
David <ruedarosasdavid@gmail.com>
2025-07-21 20:27:10 +0200

    Add new dependences and severless weather.js (Vercel deployment)

--- api/weather.js
+++ api/weather.js
@@ -1,23 +1,12 @@
-import express from 'express';
 import fetch from 'node-fetch';
-import dotenv from 'dotenv';
 
-dotenv.config();
-
-const app = express();
-const PORT = 3000;
-
-app.use(express.static('.'));
-
-app.get('/api/weather', async (_req, res) => {
+export default async function handler(req, res) {
   try {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Barcelona&units=metric&appid=${process.env.OPENWEATHER_API_KEY}`);
     const data = await response.json();
-    res.json(data);
+    res.status(200).json(data);
   } catch (error) {
     console.error('Error calling OpenWeather:', error);
     res.status(500).json({ error: 'Error calling API' });
   }
-});
-
-app.listen(PORT, () => console.log(`API calling http://localhost:${PORT}`));
+}

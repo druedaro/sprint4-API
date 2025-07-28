51bedfa1995f78fa63562aa1ab0a0da17360c19e
David <ruedarosasdavid@gmail.com>
2025-07-28 23:36:49 +0200

    Deploy on Vercel WIP 12

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./public/**/*.{js,ts,jsx,tsx}", 
-    "./components/**/*.{js,ts,jsx,tsx}"
+    './index.html',
+    './src/**/*.{js,ts}', 
   ],
   theme: {
     extend: {},

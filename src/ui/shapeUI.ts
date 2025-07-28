159ab93571520c0e73cde2a4cf2a14967787ecde
David <ruedarosasdavid@gmail.com>
2025-07-28 23:48:27 +0200

    Deploy on Vercel WIP 15

--- src/ui/shapeUI.ts
+++ src/ui/shapeUI.ts
@@ -1,7 +1,7 @@
 "use strict"
 
 export function changeBackgroundShape() {
-  const shapes = ['shape-1', 'shape-2', 'shape-3', 'shape-4', 'shape-5', 'shape-6', 'shape-7'];
+  const shapes = ['shape-1', 'shape-2', 'shape-3', 'shape-4', 'shape-5', 'shape-6'];
   const randomIndex = Math.floor(Math.random() * shapes.length);
   const selectedShape = shapes[randomIndex];
 
